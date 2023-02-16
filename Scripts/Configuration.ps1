02ffb5b92bbf0d79536a02d9994e98f1c52f3f8f
CameliaD <camelia.bobaru@gmail.com>
2023-02-16 14:20:58 +0200

    Update Configuration.ps1

--- Scripts/Configuration.ps1
+++ Scripts/Configuration.ps1
@@ -1,14 +1,3 @@
-﻿<#
--> Running this script with admin rights
-#>
-<#
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{   
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-    Start-Process powershell -Verb runAs -ArgumentList $arguments
-    Break
-}
-#>
 <#
 -> Reading the User Interface
 #>
